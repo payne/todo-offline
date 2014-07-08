cb1057e793b5f9ae1ac92d8b1c357f5a5cb5fcf1
Matt Payne <Payne@MattPayne.org>
2014-07-07 23:19:37 -0500

    Rails 4.0.4 app for keeping TODO lists offline

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TodoOffline::Application.config.secret_key_base = 'cb3f1a67316e60f871b4d43a59be3e61505c544ecc39bd7520d8f0788c603ff4cd58746023f4606f0c241fd67badab6d42aebd8dffb650a6a3f89a02c3116b95'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TodoOffline::Application.config.session_store :cookie_store, key: '_todo-offline_session'
